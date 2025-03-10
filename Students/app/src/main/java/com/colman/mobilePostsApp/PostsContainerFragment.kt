4f73dea0b5165064b83c3ad2c692787995c6a295
ayatal <ayatal.712@cs.colman.ac.il>
2025-03-10 22:55:25 +0200

    posts' list

---
+++ Students/app/src/main/java/com/colman/mobilePostsApp/PostsContainerFragment.kt
@@ -0,0 +1,33 @@
+package com.colman.mobilePostsApp
+
+import BookPostAdapter
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+class PostsContainerFragment : Fragment(R.layout.fragment_posts_container) {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: BookPostAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView = view.findViewById(R.id.recyclerViewPosts)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        adapter = BookPostAdapter(MainActivity.bookPosts)
+        recyclerView.adapter = adapter
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onResume() {
+        super.onResume()
+        adapter.notifyDataSetChanged()
+    }
+}
+
