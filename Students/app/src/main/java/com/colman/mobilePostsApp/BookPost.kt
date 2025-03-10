c63110247cea9a3a986be03b2e078378db68c253
ayatal <ayatal.712@cs.colman.ac.il>
2025-03-10 22:54:28 +0200

    book post class

---
+++ Students/app/src/main/java/com/colman/mobilePostsApp/BookPost.kt
@@ -0,0 +1,10 @@
+package com.colman.mobilePostsApp
+
+data class BookPost(
+    val userName: String,
+    val userProfile: Int,
+    val bookName: String,
+    val recommendation: String,
+    val bookImage: Int,
+    val rating: Int
+)
