9473a0c15b3707ca6d109c32cc55fd7f19410e5d
CarmelBarda <carmelbd78@colman.ac.il>
2025-03-11 12:16:39 +0200

    profile details work

--- Students/app/src/main/java/com/colman/mobilePostsApp/modules/BookPostAdapter.kt
+++ Students/app/src/main/java/com/colman/mobilePostsApp/modules/BookPostAdapter.kt
@@ -4,7 +4,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.colman.mobilePostsApp.modules.BookPost
+import com.colman.mobilePostsApp.data.BookPost
 import com.colman.mobilePostsApp.R
 
 class BookPostAdapter(
