061bfe996d345b57ba27cd23dd6e949529b6c186
ayatal <ayatal.712@cs.colman.ac.il>
2025-03-12 19:06:53 +0200

    use bookPost from firebase

--- Students/app/src/main/java/com/colman/mobilePostsApp/modules/BookPostAdapter.kt
+++ Students/app/src/main/java/com/colman/mobilePostsApp/modules/BookPostAdapter.kt
@@ -4,11 +4,12 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.colman.mobilePostsApp.modules.BookPost
 import com.colman.mobilePostsApp.R
+import com.colman.mobilePostsApp.data.bookPost.BookPost
+import com.squareup.picasso.Picasso
 
 class BookPostAdapter(
-    private val bookList: List<BookPost>
+    private var bookList: List<BookPost>
 ) : RecyclerView.Adapter<BookPostAdapter.BookViewHolder>() {
 
     inner class BookViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -29,13 +30,36 @@ class BookPostAdapter(
     override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
         val book = bookList[position]
 
-        holder.userImage.setImageResource(book.userProfile)
+        if (!book.userProfile.isNullOrEmpty()) {
+            Picasso.get()
+                .load(book.userProfile)
+                .placeholder(R.drawable.profile_pic_placeholder)
+                .error(R.drawable.profile_pic_placeholder)
+                .into(holder.userImage)
+        } else {
+            holder.userImage.setImageResource(R.drawable.profile_pic_placeholder)
+        }
+
         holder.userName.text = book.userName
         holder.bookName.text = book.bookName
         holder.recommendationText.text = book.recommendation
-        holder.bookImage.setImageResource(book.bookImage)
         holder.ratingText.text = "${book.rating}/10"
+
+        if (!book.bookImage.isNullOrEmpty()) {
+            Picasso.get()
+                .load(book.bookImage)
+                .placeholder(R.drawable.ic_book_placeholder)
+                .error(R.drawable.ic_book_placeholder)
+                .into(holder.bookImage)
+        } else {
+            holder.bookImage.setImageResource(R.drawable.ic_book_placeholder)
+        }
     }
 
     override fun getItemCount() = bookList.size
+
+    fun updatePosts(newPosts: List<BookPost>) {
+        bookList = newPosts
+        notifyDataSetChanged()
+    }
 }
