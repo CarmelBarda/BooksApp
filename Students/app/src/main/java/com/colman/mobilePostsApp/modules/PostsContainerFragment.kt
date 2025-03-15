9bf444ad3156e4cff64f9e02f4aa26c9affa266b
CarmelBarda <carmelbd78@colman.ac.il>
2025-03-15 09:17:12 +0200

    add posts spinner

--- Students/app/src/main/java/com/colman/mobilePostsApp/modules/PostsContainerFragment.kt
+++ Students/app/src/main/java/com/colman/mobilePostsApp/modules/PostsContainerFragment.kt
@@ -7,6 +7,7 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import android.widget.ProgressBar
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -20,19 +21,27 @@ import com.colman.mobilePostsApp.R
 class PostsContainerFragment : Fragment(R.layout.fragment_posts_container) {
 
     private lateinit var recyclerView: RecyclerView
+    private lateinit var progressBar: ProgressBar
     private lateinit var adapter: BookPostAdapter
     private val viewModel: BookPostViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        progressBar = view.findViewById(R.id.postsLoadingSpinner)
         recyclerView = view.findViewById(R.id.recyclerViewPosts)
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        progressBar.visibility = View.VISIBLE
+        recyclerView.visibility = View.GONE
 
         // Observe LiveData and update RecyclerView when data changes
         viewModel.bookPosts.observe(viewLifecycleOwner) { bookPosts ->
             adapter = BookPostAdapter(bookPosts)
             recyclerView.adapter = adapter
+
+            progressBar.visibility = View.GONE
+            recyclerView.visibility = View.VISIBLE
         }
 
         viewModel.refreshPosts()
